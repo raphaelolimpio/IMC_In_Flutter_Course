3821441ee928fa5a50885fc6702f0491b15dbfc6
raphaelolimpio <76962348+raphaelolimpio@users.noreply.github.com>
2024-10-01 15:36:00 -0300

    projeto finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController hieghtController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados";
+
+  void _ressetField() {
+    weightController.text = "";
+    hieghtController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    setState(
+      () {
+        double wheight = double.parse(weightController.text);
+        double height = double.parse(hieghtController.text) / 100;
+        double imc = wheight / (height * height);
+        if (imc < 18.6) {
+          _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 18.6 && imc < 24.9) {
+          _infoText = "peso ideal (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 24.9 && imc < 29.9) {
+          _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 29.9 && imc < 34.9) {
+          _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 40) {
+          _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+        }
+      },
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,41 +55,89 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _ressetField,
             icon: Icon(Icons.refresh),
           ),
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          //input number
-          const TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso(Kg)",
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
+      //colocar tudo em uma unico filho e com isso caso o teclado ou as informaçoes sejão grandes e que assim qeue assionar o teclado ele de erro, se usa o Scrollview para deixar a opção de rolar
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Icon(Icons.person_outline,
+                  size: 120.0, color: Colors.green),
+              //input number
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Peso(Kg)",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira seu Peso!";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Altura(Cm)",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: hieghtController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira a sua Altura!";
+                  }
+                },
+              ),
+              //para alinhar melhor o botão se coloca ele dentro de um container
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  // não se usa mais o RaisedButton, agora se usa o ElevatedButton
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _calculate();
+                      }
+                    },
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
+                    ),
+                    //para adicinar cor no botão se uas a linha de cod abaixo
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  ),
+                ),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25.0,
+                ),
+              ),
+            ],
           ),
-          const TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura(Cm)",
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          // não se usa mais o RaisedButton, agora se usa o ElevatedButton
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Calcular"),
-          ),
-        ],
+        ),
       ),
     );
   }
